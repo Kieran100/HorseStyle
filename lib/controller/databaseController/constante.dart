ff9507fef0cbabbb57a70a519776824196a3499a
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2022-11-23 10:26:39 +0100

    database

---
+++ lib/controller/databaseController/constante.dart
@@ -0,0 +1,5 @@
+const MONGO_URL = "mongodb+srv://admin:admin@clusterhorse.rjlmafb.mongodb.net/?retryWrites=true&w=majority";
+const COLLECTION_USER = "";
+const COLLECTION_HORSE = "";
+const COLLECTION_LESSON = "";
+const COLLECTION_EVENT = "";
---
+++ lib/controller/databaseController/mongo_db_controller.dart
@@ -0,0 +1,14 @@
+import 'dart:developer';
+
+import 'package:horsestyle/controller/databaseController/constante.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class MongoDataBaseController {
+  static connect() async{
+    var db = await Db.create(MONGO_URL);
+    await db.open();
+    inspect(db);
+  }
+
+
+}
