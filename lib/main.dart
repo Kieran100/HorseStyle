698cdb9e57a74964076ecff761f061f9bd24ab05
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2022-11-23 10:07:01 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,8 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: const <Widget>[
+            Text("Test")
           ],
         ),
       ),
