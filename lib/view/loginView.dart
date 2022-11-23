c5753ea9c3c1b37c48f9527b587a2a9347dd9f84
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2022-11-23 11:29:20 +0100

    update 1.0

--- lib/view/loginView.dart
+++ lib/view/loginView.dart
@@ -1,1 +1,31 @@
 import 'package:flutter/material.dart';
+
+class ProfilView extends StatefulWidget {
+  const ProfilView({super.key});
+
+
+
+  @override
+  State<ProfilView> createState() => _ProfilViewState();
+}
+
+class _ProfilViewState extends State<ProfilView> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Horse Style"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("Test")
+          ],
+        ),
+      ),
+    );
+  }
+}
