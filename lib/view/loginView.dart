045601c34564988179c07afe5683ad3ea1df80f6
B3r4ti0n <arcoucou77@gmail.com>
2022-11-23 12:17:26 +0100

    login

--- lib/view/loginView.dart
+++ lib/view/loginView.dart
@@ -1,35 +1,6 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-class ProfilView extends StatefulWidget {
-  const ProfilView({super.key});
 
-
-
-  @override
-  State<ProfilView> createState() => _ProfilViewState();
-}
-
-class _ProfilViewState extends State<ProfilView> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Horse Style"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text("Test")
-          ],
-        ),
-      ),
-    );
-  }
-=======
 class LoginView extends StatefulWidget{
 
   @override
@@ -38,5 +9,4 @@ class LoginView extends StatefulWidget{
     throw UnimplementedError();
   }
 
->>>>>>> alexis
 }
